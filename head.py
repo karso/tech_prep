af8ec7c95a882ccceec09224129c791474e446c9
karso <ranaghoul@gmail.com>
2020-06-08 19:36:40 +0530

    Implementation of head, incomplete

---
+++ head.py
@@ -0,0 +1,44 @@
+# This code implements linux 'head -n N' command
+import sys
+import os
+import time
+
+def realfile(file):
+    if os.path.isfile(file):
+        return file
+    else:
+        print "File", file, "does not exist"
+        exit(1)
+
+def realnum(num):
+    if num.isdigit():
+        return num
+    else:
+        usage()
+
+def usage():
+    print "python [script] [file] [num(int)]*"
+    exit(1)
+
+def head(file, lines=20):
+    stored_lines = []
+    buffer_size = 1024
+    block = 1
+    with open(file, 'r') as f:
+        while len(stored_lines) < lines:
+            stored_lines = f.readlines(buffer_size)
+    for line in stored_lines[:int(lines)]:
+        print line
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        file = realfile(sys.argv[1])
+        head(file)
+    elif len(sys.argv) == 3:
+        file = realfile(sys.argv[1])
+        lines = realnum(sys.argv[2])
+        head(file, int(lines))
+    else:
+        usage()
+
+# ToDo: Need to handle very small files or very large number of lines.        
